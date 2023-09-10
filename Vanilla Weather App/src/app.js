548dd2b7f9c65aec9282f4a6da74131eff6b7efa
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2023-09-09 21:58:59 -0400

    Added JS temperatureElement and cityElement

--- Vanilla Weather App/src/app.js
+++ Vanilla Weather App/src/app.js
@@ -1,1 +1,12 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+}
 
+let apiKey = "2a2eaa51d996796495bf456e5b58adf4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
