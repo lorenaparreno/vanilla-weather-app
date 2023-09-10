b91dd290e59852b85a6d2ace71d356b4e40f7d54
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2023-09-09 22:47:25 -0400

    Removed console.log

--- Vanilla Weather App/src/app.js
+++ Vanilla Weather App/src/app.js
@@ -23,7 +23,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
